c9010e055e9ef929ff8e086e84c1eb73bb698bad
kouhei <kouhei@2014JS02>
2016-05-26 10:08:19 +0900

    誰がどこを担当しているかを記入

--- api/addnew.php
+++ api/addnew.php
@@ -1,21 +1,21 @@
 <?php
 // 追加メインの処理
 
-//リクエストからJOSNデータを受け取る
-//JOSNデータをセットする変数 $json_data
+//リクエストからJOSNデータを受け取る　大野
+//JOSNデータをセットする変数 $json_data　
 include 'getjson.php';
 
-//JOSNデータから画像データをファイルに保存して画像ファイルの保存パスを返す。
-//画像ファイルの保存パスをセットする変数 $file_path
+//JOSNデータから画像データをファイルに保存して画像ファイルの保存パスを返す。　上田
+//画像ファイルの保存パスをセットする変数 $file_path 
 include 'savefile.php';
 
 
-//コードを発行する
-//発行したコードをセットする変数 $code
+//コードを発行する　小南
+//発行したコードをセットする変数 $code 
 include 'generatecode.php';
 
 
-//データベースに保存する
+//データベースに保存する 小森
 include 'savedata.php';
 
 
