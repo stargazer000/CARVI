aaa6b3b04d412c332b44fe5de82051c2459c8f1c
Koji Sasaki <sasaki@liquidst.jp>
2016-06-30 11:10:57 +0900

    PHPバージョン違いによる$HTTP_RAW_POST_DATAの警告抑制のための設定を追加

--- api/getjson.php
+++ api/getjson.php
@@ -1,6 +1,7 @@
 <?php
 // リクエストからJOSNデータを受け取る
 // JOSNデータ受け取る変数 $json_data
+ini_set('always_populate_raw_post_data', -1);
 $json_string = file_get_contents('php://input');
 $json_data = json_decode($json_string);
 
