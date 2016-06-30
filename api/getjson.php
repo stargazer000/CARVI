fe83ccbe765f4b91dca90afc76215fe07ab72f90
Koji Sasaki <sasaki@liquidst.jp>
2016-06-30 10:55:12 +0900

    POSTされたJSONデータを取得する

--- api/getjson.php
+++ api/getjson.php
@@ -1,8 +1,7 @@
 <?php
 // リクエストからJOSNデータを受け取る
 // JOSNデータ受け取る変数 $json_data
+$json_string = file_get_contents('php://input');
+$json_data = json_decode($json_string);
 
-
-$json_data = null;
-
-var_dump($json_data);
+//var_dump($json_data);
