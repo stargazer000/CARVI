2d6b83ddcdba02309ae01feb59fe93eeaa78b10e
kouhei <kouhei@2014JS02>
2016-08-09 16:01:02 +0900

    アプリから受け取ったコードを元にタイトル、本文、画像を検索するget.phpを作成。json化はできていない。

--- api/get.php
+++ api/get.php
@@ -1,66 +1,21 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;dbname=carvi;charset=utf8', 'root', '');
-
-print'PDO クラスによる接続に成功しました。';
-
-
-$search_key = $_GET['code'];
-
+//$search_key = filter_input( INPUT_GET, "code" );
+$search_key = 7197000705;
 try {
-    $sql = "SELECT * FROM outline WHERE code like :code";
-    $stmh = $pdo->prepare($sql);
-    $stmh->bindValue(':code', $search_key, PDO::PARAM_STR);
-    $stmh->execute();
-    $count = $stmh->rowCount();
-    print $count;
-} catch (PDOException$Exception) {
-    print $Exception->getMessage();
+    $pdo = new PDO('mysql:host=localhost;dbname=carvi;charset=utf8', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);    
+    $sql = "SELECT * FROM outline WHERE code like '%{$search_key}%';";
+    $stmt = $pdo->query($sql);
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    $id = $result['id'];
+    $sql2 = "SELECT filepath FROM media WHERE outline_id like '%{$id}%';";
+    $stmt2 = $pdo->query($sql2);
+    $result2 = $stmt2->fetch(PDO::FETCH_ASSOC);
+        echo "タイトル：".$result['title']."<br>本文:".$result['body']."<br>画像<br>";
+        echo '<img src="'.$result2['filepath'].'"/>' ;
+}catch(PDOException $Exception){
+    die('接続エラー：' . $Exception->getMessage());
 }
-if ($count < 1) {
-    print 0;
-} else {
-    while ($row = $stmh->fetch(PDO::FETCH_ASSOC)) {
-        ?>
-        <tr>
-            <td align="center"><?= htmlspecialchars($row['id']) ?></td>
-            <td><?= htmlspecialchars($row['sent_at']) ?></td>
-            <td><?= htmlspecialchars($row['title']) ?></td>
-            <td><?= htmlspecialchars($row['body']) ?></td>
-            <td align="center"><?= htmlspecialchars($row['code']) ?></td>
-        </tr>
-        <?php
-        try {
-            $sql = "SELECT * FORM media WHERE outline_id = :outline_id";
-            $stmh1 = $pdo->prepare($sql);
-            $stmh1->bindValue('outline_id', $row['id'], PDO::PARAM_INT);
-            $stmh1->execute();
-            $count = $stmh1->rowCount();
-            print $count;
-        } catch (PDOException$Exception) {
-            print $Exception->getMessage();
-        }
-        if ($count < 1) {
-            print 0;
-        } else {
-            while ($row1 = $stmh1->fetch(PDO::FETCH_ASSOC)) {
-                ?>
-                <tr>
-                    <td align="center"><?= htmlspecialchars($row1['id']) ?></td>
-                    <td><?= htmlspecialchars($row1['minetype']) ?></td>
-                    <td><?= htmlspecialchars($row1)['filepath'] ?></td>
-                    <td><?= htmlspecialchars($row1)['attributes'] ?></td>
-                    <td align="center"><?= htmlspecialchars($row1)['outline_id'] ?></td>
-                </tr>
-                }
-                }
-                }
-                ?>
-                </tbody></table>
-
-                <?php
-            }
-        }
-    }
-}
-
-            
+$pdo = null;
+?>
